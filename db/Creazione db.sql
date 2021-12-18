514cdd97408e5bec5644450e8974b716dc2acdef
Simone <simone.albonetti@studio.unibo.it>
2021-12-18 20:08:44 +0100

    Creato db

---
+++ db/Creazione db.sql
@@ -0,0 +1,220 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Dic 18, 2021 alle 20:06
+-- Versione del server: 10.4.21-MariaDB
+-- Versione PHP: 8.0.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `progettowebdb`
+--
+CREATE SCHEMA IF NOT EXISTS `progettowebdb` DEFAULT CHARACTER SET utf8 ;
+USE `progettowebdb` ;
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `articolo`
+--
+
+CREATE TABLE `articolo` (
+  `idArticolo` int(11) NOT NULL,
+  `nomeArticolo` varchar(50) NOT NULL,
+  `descrizione` mediumtext NOT NULL,
+  `taglia` varchar(10) NOT NULL,
+  `prezzo` float NOT NULL,
+  `imgArticolo` varchar(100) NOT NULL,
+  `qtaMagazzino` int(11) NOT NULL,
+  `categoria` int(11) NOT NULL,
+  `rivenditore` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `idCategoria` int(11) NOT NULL,
+  `nomeCategoria` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `idCliente` int(11) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `cognome` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `paese` varchar(30) NOT NULL,
+  `citta` varchar(30) NOT NULL,
+  `indirizzo` varchar(50) NOT NULL,
+  `civico` int(11) NOT NULL,
+  `cap` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `ordine`
+--
+
+CREATE TABLE `ordine` (
+  `idOrdine` int(11) NOT NULL,
+  `idCliente` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `rigaordine`
+--
+
+CREATE TABLE `rigaordine` (
+  `qta` int(11) NOT NULL,
+  `idArticolo` int(11) NOT NULL,
+  `idOrdine` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `rivenditore`
+--
+
+CREATE TABLE `rivenditore` (
+  `idRivenditore` int(11) NOT NULL,
+  `nome` varchar(30) NOT NULL,
+  `cognome` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `password` varchar(15) NOT NULL,
+  `piva` varchar(11) NOT NULL,
+  `citta` varchar(30) NOT NULL,
+  `indirizzo` varchar(50) NOT NULL,
+  `civico` int(11) NOT NULL,
+  `cap` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `articolo`
+--
+ALTER TABLE `articolo`
+  ADD PRIMARY KEY (`idArticolo`),
+  ADD KEY `articoloCategoria` (`categoria`),
+  ADD KEY `articoloRivenditore` (`rivenditore`);
+
+--
+-- Indici per le tabelle `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`idCategoria`);
+
+--
+-- Indici per le tabelle `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`idCliente`);
+
+--
+-- Indici per le tabelle `ordine`
+--
+ALTER TABLE `ordine`
+  ADD PRIMARY KEY (`idOrdine`),
+  ADD KEY `ordineCliente` (`idCliente`);
+
+--
+-- Indici per le tabelle `rigaordine`
+--
+ALTER TABLE `rigaordine`
+  ADD KEY `rigaOrdineOrdine` (`idOrdine`),
+  ADD KEY `rigaOrdineArticolo` (`idArticolo`);
+
+--
+-- Indici per le tabelle `rivenditore`
+--
+ALTER TABLE `rivenditore`
+  ADD PRIMARY KEY (`idRivenditore`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `articolo`
+--
+ALTER TABLE `articolo`
+  MODIFY `idArticolo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `idCategoria` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `ordine`
+--
+ALTER TABLE `ordine`
+  MODIFY `idOrdine` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT per la tabella `rivenditore`
+--
+ALTER TABLE `rivenditore`
+  MODIFY `idRivenditore` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `articolo`
+--
+ALTER TABLE `articolo`
+  ADD CONSTRAINT `articoloCategoria` FOREIGN KEY (`categoria`) REFERENCES `categoria` (`idCategoria`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `articoloRivenditore` FOREIGN KEY (`rivenditore`) REFERENCES `rivenditore` (`idRivenditore`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limiti per la tabella `ordine`
+--
+ALTER TABLE `ordine`
+  ADD CONSTRAINT `ordineCliente` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`idCliente`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limiti per la tabella `rigaordine`
+--
+ALTER TABLE `rigaordine`
+  ADD CONSTRAINT `rigaOrdineArticolo` FOREIGN KEY (`idArticolo`) REFERENCES `articolo` (`idArticolo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `rigaOrdineOrdine` FOREIGN KEY (`idOrdine`) REFERENCES `ordine` (`idOrdine`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
