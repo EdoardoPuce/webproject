e7a2c5a5ffb7e82c806d93938cb33aa5df1bf503
Edoardo.Puce <edoardo.puce@studio.unibo.it>
2021-12-18 11:15:28 +0100

    inizializzazione repo

---
+++ bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+session_start();
+define("UPLOAD_DIR", "./upload/");
+
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'bootstrap.php';
+
+//Base Template
+$templateParams["titolo"] = "Home";
+//Home Template
+
+require 'template/base.php';
+?>
