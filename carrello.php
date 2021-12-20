d20e3d5f3e9e413d9da1f0be26085afb2175db0c
Edoardo.Puce <edoardo.puce@studio.unibo.it>
2021-12-20 15:10:09 +0100

    aggiunta pagina carrello e relativo css

---
+++ carrello.php
@@ -0,0 +1,12 @@
+<?php
+require_once 'bootstrap.php';
+
+//Base Template
+$templateParams["titolo"] = "Carrello";
+$templateParams["nome"] = "carrello.php";
+
+//Home Template
+
+
+require 'template/base.php';
+?>
---
+++ template/carrello.php
@@ -0,0 +1,20 @@
+<link rel="stylesheet" type="text/css" href="./css/carrello.css" />
+<section>
+    <h1>Carrello</h1>
+    <div class="carrello">
+        
+    </div>
+    <div class="dati">
+        <div>
+
+        </div>
+        <div>
+            <div>
+
+            </div>
+            <div>
+
+            </div>
+        </div>
+    </div>
+</section>
