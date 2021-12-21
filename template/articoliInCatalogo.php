eb935b96f0dece6ababa0c861f78815cb5ea92dd
Simone <simone.albonetti@studio.unibo.it>
2021-12-21 14:55:37 +0100

    Aggiunto articoli in db, migliorato css

--- template/articoliInCatalogo.php
+++ template/articoliInCatalogo.php
@@ -1,14 +1,25 @@
 <link rel="stylesheet" type="text/css" href="./css/catalogo.css" />
 
 
-<?php foreach ($templateParams['articoli'] as $articolo): ?>
+<?php foreach ($templateParams['articoli'] as $articolo) : ?>
     
-    <div class = "articolo">
-        <img class ="prodotto" src= './upload/packaging.png'/>
-        <p class = "prezzo"> <?php echo($articolo["prezzo"]) ?> </p>
-        <p class = "nome"> <?php echo($articolo["nomeArticolo"]) ?> </p>
-        <p class = "disponibilità"> disponibilità immediata </p>
+    <?php $qta = $articolo["qtaMagazzino"];
+            if ($qta > 5) {
+                $risultato = "Disponibile";
+            } else if ($qta > 0 && $qta <= 5) {
+                $risultato = "Ultimi pezzi";
+            } else {
+                $risultato = "Non disponibile";
+            } ?> 
+    
+    <div class="articolo">
+        <img class="prodotto" src='./upload/packaging.png' />
+        <p class="prezzo">€ <?php echo ($articolo["prezzo"]) ?> </p>
+        <p class="nome"> <?php echo ($articolo["nomeArticolo"]) ?> </p>
+        
+        <p class="disponibilità" value="<?php echo $risultato?>">Disponibilita: <?php echo $risultato?>
+            </p>
     </div>
-   
-   
-<?php endforeach; ?>
+
+
+<?php endforeach;
