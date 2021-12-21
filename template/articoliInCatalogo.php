915b1020db0370189d2331c890b5288beee26076
Simone <simone.albonetti@studio.unibo.it>
2021-12-21 23:32:25 +0100

    Aggiunte foto agli articoli

--- template/articoliInCatalogo.php
+++ template/articoliInCatalogo.php
@@ -1,25 +1,34 @@
 <link rel="stylesheet" type="text/css" href="./css/catalogo.css" />
 
+<section>
+    <form>
+        <input type = "image" src ="./upload/search.png" alt = "Submit" width="48" height = "48" />
+        <label>Cerca per nome: <input type = "text" name = "ricerca"/>
+    </form>
 
+</section>
+
+
+<section>
 <?php foreach ($templateParams['articoli'] as $articolo) : ?>
     
     <?php $qta = $articolo["qtaMagazzino"];
             if ($qta > 5) {
-                $risultato = "Disponibile";
+                $disponibilità = "Disponibile";
             } else if ($qta > 0 && $qta <= 5) {
-                $risultato = "Ultimi pezzi";
+                $disponibilità = "Ultimi pezzi";
             } else {
-                $risultato = "Non disponibile";
+                $disponibilità = "Non disponibile";
             } ?> 
     
     <div class="articolo">
-        <img class="prodotto" src='./upload/packaging.png' />
+        <img class="prodotto" src='<?php echo UPLOAD_DIR.$articolo["imgArticolo"]; ?>' alt ="" />
         <p class="prezzo">€ <?php echo ($articolo["prezzo"]) ?> </p>
         <p class="nome"> <?php echo ($articolo["nomeArticolo"]) ?> </p>
         
-        <p class="disponibilità" value="<?php echo $risultato?>">Disponibilita: <?php echo $risultato?>
+        <p class="disponibilità" value="<?php echo $disponibilità?>"> <?php echo $disponibilità?>
             </p>
     </div>
 
-
-<?php endforeach;
+<?php endforeach;?>
+</section>
