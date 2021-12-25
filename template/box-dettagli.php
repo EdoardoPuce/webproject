6dd031ebfdf3c695233edbf0180c3a48b7c61b4b
Edoardo.Puce <edoardo.puce@studio.unibo.it>
2021-12-25 19:44:14 +0100

    piccola modifica

--- template/box-dettagli.php
+++ template/box-dettagli.php
@@ -3,7 +3,7 @@
 <article>
     <header>
         <img src="./upload/packaging.png" alt="" />
-        <h2 for="nome">Ordine: 2</h2>
+        <h3 for="nome">Ordine: 2</h3>
     </header>
     <section>
         <input type="button" name="Dettagli" value="dettagli" />
