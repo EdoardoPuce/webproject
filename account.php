41b73dd7b97b478d4764c065908b7c7ccbbc319c
Edoardo.Puce <edoardo.puce@studio.unibo.it>
2021-12-25 12:42:29 +0100

    aggiunta pagina account

---
+++ account.php
@@ -0,0 +1,13 @@
+<?php 
+require_once 'bootstrap.php';
+
+$templateParams["titolo"] = "Account";
+$templateParams["nome"] = "account.php";
+
+//Home Template
+
+//Base template
+
+
+require_once "template/base.php";
+?>
---
+++ template/account.php
@@ -0,0 +1,17 @@
+<link rel="stylesheet" type="text/css" href="./css/account.css" />
+
+<section>
+    <h1>Account</h1>
+    <aside>
+        <a href="#">Dati</a>
+        <div></div>
+        <a href="#">Ordini</a>
+        <div></div>
+        <a href="#">Gestione Articoli</a>
+    </aside>
+    <section>
+        <?php 
+            require("box-dettagli.php");
+        ?>
+    </section>
+</section>
