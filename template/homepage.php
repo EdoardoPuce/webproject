93e068e545ccc89b95cc94877ee7eae0e22c9b44
Thomas Testa <thomas.testa@studio.unibo.it>
2021-12-25 12:31:36 +0100

    aggiunta homepage

---
+++ template/homepage.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="utf-8" />
+    <title> Homepage </title>
+    <link type="text/css" rel="stylesheet"  href="homepage.css" />
+</head>
+    <body>
+        <header><img src="hiking.png" ></header>
+        <nav><h1><a href="Homepage.php"> Homepage </a></h1>
+            <h2><a href= "Catalogo.php"> Catalogo </a></h2>
+            <h3> img</h3>
+            <h4> img </h4>
+            <h5> img </h5>
+        </nav>
+        <h> Scopri tutto l'abbigliamento e l'attrezzatura da trekking, lasciati ispirare dai nostri consigli e resta aggiornato sulle novità.</h>
+        <hr>
+        <section>
+            <p> Zaino </p>
+            <p1> Oggi miglior prezzo </p1> 
+        </section>
+        <hr>
+        <br>
+        <section>
+            <h>Articoli più in voga </h>
+        </section>
+        <br>
+        <section>
+            <h> Catalogo </h>
+            <p> Abbigliamneto da tracking</p>
+            <ul>
+                <li> Cappelli</li>
+                <li> Maglie </li>
+                <li> Pantaloni </li>
+                <li> Guanti </li>
+                <li> Calze </li>
+                <p> Vedi tutti </p>
+           </ul>
+           <p> Equipaggiamento da trecking</p>
+           <ul>
+                <li> Zaini </li>
+                <li> Occhiali </li>
+                <li> Binocoli </li>
+                <li> Bussola </li>
+                <li> Orologi </li>
+                <p> Vedi tutti </p>
+           </ul>
+           <p> Attrezzatura da trecking</p>
+            <ul>
+                <li> Tende Trekking  </li>
+                <li> Materassini </li>
+                <li> Fornelli </li>
+                <li> Pasti pronti </li>
+                <li> Stoviglie </li>
+                <li> Posate </li>
+                <p> Vedi tutti </p>
+           </ul>
+            <br>
+            <br>
+            <p> Scopri il catalogo completo</p>
+        </section>
+        <hr>
+        
+    </body>
