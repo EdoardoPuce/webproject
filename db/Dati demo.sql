686d83ac55c0f6f053bf90f31a0ca57bc353a08b
Simone <simone.albonetti@studio.unibo.it>
2022-01-05 18:32:18 +0100

    Aggiunto campo visualizzato in tabella ordini per notifiche

--- db/Dati demo.sql
+++ db/Dati demo.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Dic 29, 2021 alle 21:51
+-- Creato il: Gen 05, 2022 alle 18:21
 -- Versione del server: 10.4.21-MariaDB
 -- Versione PHP: 8.0.12
 
@@ -31,26 +31,24 @@ INSERT INTO `categoria` (`idCategoria`, `nomeCategoria`) VALUES
 (3, 'Felpe'),
 (4, 'T-shirt');
 
-
 --
 -- Dump dei dati per la tabella `rivenditore`
 --
 
 INSERT INTO `rivenditore` (`idRivenditore`, `nome`, `cognome`, `email`, `password`, `piva`, `citta`, `indirizzo`, `civico`, `cap`) VALUES
 (1, 'Mario', 'Bianchi', 'mario.bianchi@gmail.com', 'webproject', '12345678901', 'Cesena', 'Via dell\'università', 50, 47521);
-COMMIT;
-
 
 --
 -- Dump dei dati per la tabella `articolo`
 --
 
 INSERT INTO `articolo` (`idArticolo`, `nomeArticolo`, `descrizione`, `taglia`, `prezzo`, `imgArticolo`, `qtaMagazzino`, `categoria`, `rivenditore`) VALUES
-(2, 'Scaldacollo', 'Protegge dalle intemperie', 'unisex', 15, 'paracollo.png', 30, 1, 1),
+(2, 'Scaldacollo', 'Protegge dalle intemperie', 'unisex', 15.5, 'paracollo.png', 30, 1, 1),
 (3, 'Berretta uomo', 'Tieni al caldo i pensieri', 'unisex', 25, 'berrettaUomo.png', 20, 1, 1),
 (4, 'Berretta donna', 'Tieni al caldo i pensieri', 'unisex', 25, 'berrettaDonna.png', 3, 1, 1),
 (5, 'Pantaloni uomo', 'descrizione pantaloni uomo', 'M', 25, 'pantaloniUomo.jpg', 4, 2, 1),
-(6, 'Pantaloni donna', 'descrizione pantaloni donna', 'S', 2, 'pantaloniDonna.jpg', 0, 2, 1);
+(6, 'Pantaloni donna', 'descrizione pantaloni donna', 'S', 2, 'pantaloniDonna.jpg', 0, 2, 1),
+(12, 'Felpa montagna uomo NH100 Hybrid blu', 'Questa felpa con cappuccio calda e confortevole è il prodotto ideale in qualunque occasione. Tasca ventrale.\r\n\r\nI nostri ideatori appassionati di montagna e trekking hanno sviluppato questa felpa antivento per le tue escursioni nella natura.\r\n\r\nLa nostra motivazione?Offrirti il massimo del comfort.Puoi anche usare questa felpa sotto una giacca in caso di pioggia.Ha una protezione anti vento sul busto e un pile morbido smerigliato all\'interno', 'M', 24.99, 'felpa-montagna-uomo-nh100-hybrid-blu.jpg', 30, 3, 1);
 
 
 --
@@ -64,9 +62,9 @@ INSERT INTO `cliente` (`idCliente`, `nome`, `cognome`, `email`, `password`, `pae
 -- Dump dei dati per la tabella `ordine`
 --
 
-INSERT INTO `ordine` (`idOrdine`, `idCliente`, `stato`) VALUES
-(1, 1, 0),
-(2, 1, 0);
+INSERT INTO `ordine` (`idOrdine`, `idCliente`, `stato`, `visualizzato`) VALUES
+(1, 1, 0, b'0'),
+(2, 1, 0, b'0');
 
 --
 -- Dump dei dati per la tabella `rigaordine`
@@ -74,9 +72,11 @@ INSERT INTO `ordine` (`idOrdine`, `idCliente`, `stato`) VALUES
 
 INSERT INTO `rigaordine` (`qta`, `idArticolo`, `idOrdine`) VALUES
 (2, 2, 1),
-(1, 3, 2);
+(1, 3, 2),
+(4, 4, 1);
 
 
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
