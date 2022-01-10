d45af46e86a84fa50b3936db8c73fb9a77fdfc1a
Simone <simone.albonetti@studio.unibo.it>
2022-01-10 21:34:29 +0100

    Risolto bug 2 volte login

--- login.php
+++ login.php
@@ -1,51 +1,40 @@
 <?php
 require_once 'bootstrap.php';
 
-if(isUserLoggedIn()){
-    if(isset($_GET['lg']) && $_GET['lg']==1 ){
+if (isUserLoggedIn()) {
+    if (isset($_GET['lg']) && $_GET['lg'] == 1) {
         logout();
     }
+    $templateParams["nome"] = "account.php?pg=1";
 }
 
-    $templateParams["titolo"] = "Login";
-    $templateParams["nome"] = "login-form.php";
+$templateParams["titolo"] = "Login";
+$templateParams["nome"] = "login-form.php";
 
+if (isset($_POST["submit"])) {
+    if (isset($_POST["email"]) && isset($_POST["password"])) {
+        if (isset($_POST["utente"]) && $_POST['utente'] == "1") {
+            $login_result = $dbh->checkLogin($_POST['utente'], $_POST["email"], $_POST["password"]);
+            if (count($login_result) == 0) {
+                echo '<script>alert("Errore! Email o password errate!")</script>';
+            } else {
+                $_SESSION['value'] = 1;   //salvo cliente
+                registerLoggedUser($login_result[0]);
+                header("location: account.php?pg=1");
+            }
+        } elseif (isset($_POST["utente"]) && $_POST['utente'] == '0') {
 
-//fase di login
-if(isset($_POST["email"]) && isset($_POST["password"]) && isset($_POST["submit"]) ){ 
-    if (isset($_POST["utente"]) && $_POST['utente'] == "1"){
-        $login_result = $dbh->checkLogin($_POST['utente'],$_POST["email"], $_POST["password"]);
-        if(count($login_result)==0){
-            echo '<script>alert("Errore! Email o password errate!")</script>';
+            $login_result = $dbh->checkLogin($_POST['utente'], $_POST["email"], $_POST["password"]);
+            if (count($login_result) == 0) {
+                echo '<script>alert("Errore! Email o password errate!")</script>';
+            } else {
+                $_SESSION['value'] = 0;    // salvo rivenditore
+                registerLoggedUser($login_result[0]);
+                header("location: account.php?pg=1");
+            }
         } else {
-            registerLoggedUser($login_result[0]);
-
-            $_SESSION['value'] = 1;   //salvo cliente
-            
-            header("location: account.php?pg=1");
-            var_dump($_REQUEST);
+            echo '<script>alert("Si prega di fare una scelta")</script>';
         }
-
-    } elseif (isset($_POST["utente"]) && $_POST['utente'] == '0'){
-
-        $login_result = $dbh->checkLogin($_POST['utente'],$_POST["email"], $_POST["password"]);
-        if(count($login_result)==0){
-            echo '<script>alert("Errore! Email o password errate!")</script>';
-        } else {
-            registerLoggedUser($login_result[0]);
-
-            $_SESSION['value'] = 0;    // salvo rivenditore
-            header("location: account.php?pg=1");
-        }
-    } else {
-        echo '<script>alert("Si prega di fare una scelta")</script>';
     }
-
-    
-
 }
-
 require 'template/base.php';
-?>
-
-
