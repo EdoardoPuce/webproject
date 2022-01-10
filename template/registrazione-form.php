4786f57dccf4e0f24e8506f1f30f9d37d6959da4
Simone <simone.albonetti@studio.unibo.it>
2022-01-10 19:01:56 +0100

    Rimosso pulsante annulla in registrazione-form.php

--- template/registrazione-form.php
+++ template/registrazione-form.php
@@ -7,40 +7,39 @@
             <h3> Sei un cliente o un rivenditore?</h3>
             <fieldset>
                 <legend>Scegli tipologia utente:</legend>
-            <label for="cliente"><input type="radio" id="cliente" name="utente" value="1">
-                Cliente </label>
-            <label for="rivenditore"><input type="radio" id="rivenditore" name="utente" value="0">
-                Rivenditore </label>
+                <label for="cliente"><input type="radio" id="cliente" name="utente" value="1">
+                    Cliente </label>
+                <label for="rivenditore"><input type="radio" id="rivenditore" name="utente" value="0">
+                    Rivenditore </label>
             </fieldset>
             <fieldset>
                 <legend> Inserisci i dati:</legend>
                 <label for="nome"> Nome:
                     <input type="text" id="nome" name="nome" /></label>
                 <label for="cognome"> Cognome:
-                    <input type="text" name="cognome" id = "cognome"/></label>
+                    <input type="text" name="cognome" id="cognome" /></label>
                 <label class="riv" for="piva"> P.iva:
-                    <input type="text" name="piva" id = "piva" />
+                    <input type="text" name="piva" id="piva" />
                 </label>
                 <label class="clien" for="paese">Paese:
-                    <input type="text" name="paese" id = "paese" />
+                    <input type="text" name="paese" id="paese" />
                 </label>
                 <label for="citta"> Città:
-                    <input type="text" name="citta" id = "citta"/></label>
+                    <input type="text" name="citta" id="citta" /></label>
                 <label for="indirizzo"> Indirizzo:
-                    <input type="text" name="indirizzo" id = "indirizzo"/></label>
+                    <input type="text" name="indirizzo" id="indirizzo" /></label>
                 <label for="civico">Civico:
-                    <input type="number" name="civico" id = "civico"></label>
+                    <input type="number" name="civico" id="civico"></label>
                 <label for="cap"> Cap:
-                    <input type="number" name="cap" id ="cap" /></label>
+                    <input type="number" name="cap" id="cap" /></label>
                 <label for="email"> Email:
-                    <input type="email" name="email" id = "email"/></label>
+                    <input type="email" name="email" id="email" /></label>
                 <label for="password"> Password:
-                    <input type="password" name="password" id ="password"/></label>
+                    <input type="password" name="password" id="password" /></label>
                 <label for="conferma_password"> Conferma password:
-                    <input type="password" name="conferma_password" id ="conferma_password"/></label>
+                    <input type="password" name="conferma_password" id="conferma_password" /></label>
             </fieldset>
             <button type="submit" name="submit" value="Registrati">Registrati</button>
-            <button onclick="location.href ='login.php'">Annulla</button>
         </form>
     </div>
 </section>
