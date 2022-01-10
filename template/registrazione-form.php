62821633c37fc32815aa6e39a13f4fab93a4f444
Thomas Testa <thomas.testa@studio.unibo.it>
2022-01-10 17:46:30 +0100

    modifiche varie

--- template/registrazione-form.php
+++ template/registrazione-form.php
@@ -1,10 +1,6 @@
-
-
-
 <link rel="stylesheet" type="text/css" href="./css/registrazione.css" />
 <script src='./js/registrazione.js' type="text/javascript"></script>
 <section>
-
     <h2> Registrati! </h2>
     <div class="registrazione">
         <form method="POST">
@@ -15,45 +11,36 @@
             <label for="rivenditore"> Rivenditore </label>
             <fieldset>
                 <legend> Inserisci i dati:</legend>
-                <label> Nome:
-                    <input type="text" id="nome" name="nome" />
-                </label>
-                <label> Cognome:
-                    <input type="text" name="cognome" />
-                </label>
+                <label> Nome:</label>
+                <input type="text" id="nome" name="nome" />
+                <label> Cognome:</label>
+                <input type="text" name="cognome" />
                 <label class="riv"> P.iva:
-                    <input type="text" name="piva" />
+                <input type="text" name="piva" />
                 </label>
                 <label class="clien">Paese:
-                    <input type="text" name="paese" />
+                <input type="text" name="paese" />
                 </label>
-                <label> Città:
-                    <input type="text" name="citta" />
-                </label>
-                <label> Indirizzo:
-                    <input type="text" name="indirizzo" />
-                </label>
-                <label>Civico:
-                    <input type="number" name="civico">
-                </label>
-                <label> Cap:
-                    <input type="number" name="cap" />
-                </label>
-                <label> Email:
-                    <input type="email" name="email" />
-                </label>
-                <label> Password:
-                    <input type="password" name="password" />
-                </label>
-                <label> Conferma password:
-                    <input type="password" name="conferma_password" />
-                </label>
+                <label> Città:</label>
+                <input type="text" name="citta" />
+                <label> Indirizzo:</label>
+                <input type="text" name="indirizzo" />
+                <label>Civico:</label>
+                <input type="number" name="civico">
+                <label> Cap:</label>
+                <input type="number" name="cap" />
+                <label> Email:</label>
+                <input type="email" name="email" />
+                <label> Password:</label>
+                <input type="password" name="password" />
+                <label> Conferma password:</label>
+                <input type="password" name="conferma_password" />
             </fieldset>
             <button type="submit" name="submit" value="Registrati">Registrati</button>
-            <button type="submit" name="submit" value="Annulla">Annulla </button>
+            <button onclick="location.href ='login.php'">
+            Annulla
+           </button>
         </form>
     </div>
-
-
 </section>
 
