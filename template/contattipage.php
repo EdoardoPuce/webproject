9222e56ae292c557ba1f019d3ae9f1c73f4c8225
Thomas Testa <thomas.testa@studio.unibo.it>
2022-01-10 17:38:56 +0100

    contatti mod

--- template/contattipage.php
+++ template/contattipage.php
@@ -3,38 +3,39 @@
 <section>
     <h2> CONTATTI DELLO STORE </h2>
     <p> Il nostro Servizio Clienti sarà a tua disposizione per ogni dubbio, perplessità o richiesta d’aiuto. <br>
-    Non esitare a contattarci tramite i canali di comunicazione ed assistenza dedicata elencati di seguito.</p>
+        Non esitare a contattarci tramite i canali di comunicazione ed assistenza dedicata elencati di seguito.</p>
     <div class="colonna">
-        <img src="telefono.png" alt="telefono">
+        <img src="./upload/telefono.png" alt="telefono">
         <h3> Telefono</h3>
         <hr>
         <p> Servizio clienti</p>
         <p> Puoi contattare il servizio clienti al numero 000000000<br>
-        Attivo dalle 09:00 alle 22:00, 7 giorni su 7</p>
+            Attivo dalle 09:00 alle 22:00, 7 giorni su 7</p>
     </div>
     <div class="colonna">
-        <img src="email.jpg"indiri alt="email">
+        <img src="./upload/email.png"indiri alt="email">
         <h3>Email</h3>
         <hr> 
         <p> Assistenza clienti</p>
         <a href="assistenza.trekking@gmail.com">assistenza.trekking@gmail.com </a>
         <p>Per qualsiasi problematica tu abbia bisogno,<br>
-        la nostra assitenza sarà a tua completa disposizione.</p>
+           la nostra assitenza sarà a tua completa disposizione.</p>
     </div>
     <div class="colonna">
-        <img src="indirizzopostaordinaria.png" alt="indirizzo">
+        <img src="./upload/indirizzopostaordinaria.png" alt="indirizzo">
         <h3> Indirizzo di <br>
             posta ordinaria</h3>
         <hr>
         <p> Trekking S.p.A.  </p>
-        <p> Via dell'università 50 <br>
-            47521, cesena(FC)- Italia<br>
+        <p> Via dell'Università 50 <br>
+            47521, Cesena(FC)- Italia<br>
             Il servizio è attivo 24/24, 7 giorni su 7.</p>
     </div>
     <div class="colonna">
-        <img src="chat.jpg" alt="chat">
+        <img src="./upload/chat.png" alt="chat">
         <h3> Live chat</h3>
         <hr>
-        <p> La nostra Live Chat è disponibile soltanto dopo aver effettuato il Login. Uno dei nostri operatori ti fornirà tempestivamente tutto il supposto necessario.</p>
+        <p> La nostra Live Chat è in fase di sviluppo. <br>
+            Sarà disponibile in futuro.</p>
     </div>
-</section>
+</section> 
